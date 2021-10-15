489b0963c44609dd8c90e6ca6b6643e3d9acbf72
junhee916 <junhee916@naver.com>
2021-10-15 13:18:40 +0900

    form_post_ejs_connect

---
+++ comm_modules/user.js
@@ -0,0 +1,12 @@
+const signupBtn = document.getElementById("signupBtn")
+
+signupBtn.addEventListener('click', function(){
+
+    const name = document.getElementById("name")
+
+    const email = document.getElementById("email")
+
+    const password = document.getElementById("password")
+
+    
+})
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
@@ -10,6 +11,12 @@ const commendRouter = require('./routes/commend')
 
 const connectDB = require('./config/database')
 connectDB()
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/comm_modules', express.static(__dirname + '/comm_modules'))
+app.use('/public', express.static(__dirname + '/public'))
 
 // middleware
 app.use(bodyParser.json())
@@ -25,6 +32,16 @@ app.use('/commend', commendRouter)
 // connected uploads
 app.use('/uploads', express.static('uploads'))
 
+// connected views
+app.get('/signup', (req, res) => {
+
+    res.render('./user/signup')
+})
+
+app.get('/login', (req, res) => {
+
+    res.render('./user/login')
+})
 
 const PORT = process.env.PORT || 7000
 
