fc168d0ac9f6198f48a7bd5d3672efa7d72728f2
junhee916 <junhee916@naver.com>
2021-10-15 12:32:17 +0900

    noticeBoard_mongodb_restApi

---
+++ middleware/check_auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function(req, res, next){
+
+    try{
+
+        const token = req.headers.authorization.split(' ')[1]
+
+        const decode = jwt.verify(token, process.env.SECRET_KEY)
+
+        req.userData = decode
+
+        next();
+    }
+    catch(err){
+        res.status(500).json({
+            msg : err.message
+        })
+    }
+}
